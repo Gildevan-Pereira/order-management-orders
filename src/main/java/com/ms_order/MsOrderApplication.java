76f97bdd2948ce3de20b4b8df8e329640544fb2b
GildevanPereira <gildevanpereira.pro@gmail.com>
2024-11-13 13:09:09 -0300

    Ajustando cobertura

--- src/main/java/com/ms_order/MsOrderApplication.java
+++ src/main/java/com/ms_order/MsOrderApplication.java
@@ -1,10 +1,12 @@
 package com.ms_order;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@Generated
 @EnableJpaAuditing
 @EnableMongoAuditing
 @SpringBootApplication
